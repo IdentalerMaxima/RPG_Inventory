fd509ca764f3352d02c8a0887717301807f143b7
Náray Koppány <koppany.naray@gmail.com>
2022-05-22 23:55:07 +0200

    Remade constructors for a non-magic and magic one.

--- src/main/java/business/Armor.java
+++ src/main/java/business/Armor.java
@@ -4,12 +4,18 @@ import business.os.Item;
 
 public class Armor extends Item {
 
-    int armorAmount = 10;
+    int armorAmount;
+    magicTypeEnum magicType;
 
-    public Armor(String name, Boolean isMagic, int weight, int durability, Integer armorAmount) {
-        super(name, isMagic, weight, durability);
+    public Armor(String name, int weight, int durability, int armorAmount) { //non-magic
+        super(name, weight, durability);
         this.armorAmount = armorAmount;
+    }
 
+    public Armor(String name, String magicType, int weight, int durability, int armorAmount) {
+        super(name, weight, durability);
+        this.armorAmount = armorAmount;
+        this.magicType = magicTypeEnum.valueOf(magicType);
     }
 
 }
--- src/main/java/business/Weapon.java
+++ src/main/java/business/Weapon.java
@@ -4,12 +4,19 @@ import business.os.Item;
 
 public class Weapon extends Item {
 
-    int damage = 10;
+    int damage;
+    magicTypeEnum magicType;
 
-    public Weapon(String name, Boolean isMagic, int weight, int durability, Integer damage) {
-        super(name, isMagic, weight, durability);
+    public Weapon(String name, int weight, int durability, int damage) { //non-magic
+        super(name, weight, durability);
         this.damage = damage;
 
+    }
+
+    public Weapon(String name, String magicType, int weight, int durability, int damage) { //magic
+        super(name, weight, durability);
+        this.damage = damage;
+        this.magicType = magicTypeEnum.valueOf(magicType);
     }
 
 }
