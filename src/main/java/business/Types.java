da90c546d469d9b2d33e46879ff91c68437c20d3
Náray Koppány <koppany.naray@gmail.com>
2022-05-22 17:57:21 +0200

    Made enums and their damageBonus, added getDamageBonus function

--- src/main/java/business/Types.java
+++ src/main/java/business/Types.java
@@ -1,4 +1,17 @@
 package business;
 
+import java.lang.reflect.Type;
+
 public enum Types {
+    FIRE (2), WATER(2), EARTH(2), AIR(2), LIGHT(4), DARK(5), NONE(1);
+
+    private final Integer damageBonus;
+
+    Types(Integer damageBonus) { //elv itt felesleges a lathatosagot megadni
+        this.damageBonus = damageBonus;
+    }
+
+    public Integer getDamageBonus() {
+        return this.damageBonus;
+    }
 }
